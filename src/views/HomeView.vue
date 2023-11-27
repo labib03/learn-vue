7d98933d0c1021cfd64af59549b52fd76ace280f
labib dev <ansorudinlabib1@gmail.com>
2023-11-27 16:31:02 +0700

    init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Home View</h1>
+      <p>{{ counter.count }}</p>
+      <button @click="counter.increment">Increment</button>
+    </div>
+  </main>
+</template>
